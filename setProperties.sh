68753077783170b0b021139024cab5a92f66cefd
MrMiyagi33 <97127664+MrMiyagi33@users.noreply.github.com>
2024-08-17 09:49:47 -0400

    better handling with white space

--- setProperties.sh
+++ setProperties.sh
@@ -1,21 +1,19 @@
 export LD_LIBRARY_PATH=.
 
-srvName=$1
-gMode=$2
-dif=$3
-allow=$4
-sPort=$5
-sPort6=$6
-name=$7
-seed=$8
+srvName="$1"
+gMode="$2"
+dif="$3"
+allow="$4"
+sPort="$5"
+sPort6="$6"
+name="$7"
+seed="$8"
 
-sed -i -e "s/\(^server-name=\).*/\1$1/" \
--e "s/\(^gamemode=\).*/\1$2/" \
--e "s/\(^difficulty=\).*/\1$3/" \
--e "s/\(^allow-list=\).*/\1$4/" \
--e "s/\(^server-port=\).*/\1$5/" \
--e "s/\(^server-portv6=\).*/\1$6/" \
--e "s/\(^level-name=\).*/\1$7/" \
--e "s/\(^level-seed=\).*/\1$8/" server.properties
-
-./bedrock_server
+sed -i -e "s/\(^server-name=\).*/\1$srvName/" \
+-e "s/\(^gamemode=\).*/\1$gMode/" \
+-e "s/\(^difficulty=\).*/\1$dif/" \
+-e "s/\(^allow-list=\).*/\1$allow/" \
+-e "s/\(^server-port=\).*/\1$sPort/" \
+-e "s/\(^server-portv6=\).*/\1$sPort6/" \
+-e "s/\(^level-name=\).*/\1$name/" \
+-e "s/\(^level-seed=\).*/\1$seed/" server.properties
