1ed78baba062acfcd4b8410201c75742aabd71e0
MrMiyagi33 <97127664+MrMiyagi33@users.noreply.github.com>
2024-08-17 10:36:52 -0400

    add echo

--- setProperties.sh
+++ setProperties.sh
@@ -2,14 +2,25 @@
 export LD_LIBRARY_PATH=.
 
 srvName="$1"
+echo $srvName
 gMode="$2"
+echo $gMode
 dif="$3"
+echo $dif
 allow="$4"
+echo $allow
 sPort="$5"
+echo $sPort
 sPort6="$6"
+echo $sPort6
 name="$7"
+echo $name
 seed="$8"
+echo $seed
 serverCore="$9"
+echo $serverCore
+
+echo 'starting replacement of properties'
 
 sed -i -e "s/\(^server-name=\).*/\1$srvName/" \
 -e "s/\(^gamemode=\).*/\1$gMode/" \
@@ -19,5 +30,8 @@ sed -i -e "s/\(^server-name=\).*/\1$srvName/" \
 -e "s/\(^server-portv6=\).*/\1$sPort6/" \
 -e "s/\(^level-name=\).*/\1$name/" \
 -e "s/\(^level-seed=\).*/\1$seed/" server.properties
+
+echo 'replacement complete'
+echo 'starting server engine'
 
 exec $serverCore
